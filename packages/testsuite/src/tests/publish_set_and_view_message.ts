e5910f216c1efe934d75a631f370b25aa217a2d0
Victor Gao <10379359+vgao1996@users.noreply.github.com>
2025-12-09 16:38:46 -0800

    add checks for lockfiles and formatting (#5)

--- packages/testsuite/src/tests/publish_set_and_view_message.ts
+++ packages/testsuite/src/tests/publish_set_and_view_message.ts
@@ -2,35 +2,35 @@ import { TestHarness, assertTxnSuccess } from "forklift";
 import * as path from "path";
 
 describe("publish, set message, and view message", () => {
-    let harness: TestHarness = new TestHarness();
+  let harness: TestHarness = new TestHarness();
 
-    it("publish package", () => {
-        const packageDir = path.join(__dirname, "../../move_packages/message");
-        const publishRes = harness.publishPackage({
-            profile: "default",
-            packageDir,
-            namedAddresses: {
-                simple_message: "default",
-            },
-        });
-        assertTxnSuccess(publishRes)
+  it("publish package", () => {
+    const packageDir = path.join(__dirname, "../../move_packages/message");
+    const publishRes = harness.publishPackage({
+      profile: "default",
+      packageDir,
+      namedAddresses: {
+        simple_message: "default",
+      },
     });
+    assertTxnSuccess(publishRes);
+  });
 
-    const message = "Hello, Aptos!";
-    it("set message", () => {
-        const runRes = harness.runMoveFunction({
-            profile: "default",
-            functionId: `default::message::set_message`,
-            args: [`string:${message}`],
-        });
-        assertTxnSuccess(runRes);
+  const message = "Hello, Aptos!";
+  it("set message", () => {
+    const runRes = harness.runMoveFunction({
+      profile: "default",
+      functionId: `default::message::set_message`,
+      args: [`string:${message}`],
     });
+    assertTxnSuccess(runRes);
+  });
 
-    it("view message", () => {
-        const viewRes = harness.runViewFunction({
-            functionId: `default::message::get_message`,
-            args: [`address:default`],
-        });
-        expect(viewRes.Result[0]).toBe(message);
+  it("view message", () => {
+    const viewRes = harness.runViewFunction({
+      functionId: `default::message::get_message`,
+      args: [`address:default`],
     });
+    expect(viewRes.Result[0]).toBe(message);
+  });
 });
